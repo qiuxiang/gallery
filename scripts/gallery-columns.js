c06ddd57ac25f1b952ae9fc2b408f2e2986acb74
Qiu Xiang <xiang.qiu@qq.com>
2016-04-13 16:21:16 +0800

    优化模块复用

--- scripts/gallery-columns.js
+++ scripts/gallery-columns.js
@@ -8,11 +8,10 @@
 var GalleryColumns = function (selector, col) {
   this.element = document.querySelector(selector)
   this.col = col || 5
-  this.initColumns()
-  this.initModal()
+  this.init()
 }
 
-GalleryColumns.prototype.initColumns = function () {
+GalleryColumns.prototype.init = function () {
   var width = this.element.clientWidth / this.col
   var html = ''
   for (var i = 0; i < this.col; i += 1) {
@@ -22,57 +21,13 @@ GalleryColumns.prototype.initColumns = function () {
   this.columns = this.element.querySelectorAll('.gallery-column')
 }
 
-GalleryColumns.prototype.initModal = function () {
-  this.modal = document.createElement('div')
-  this.modal.className = 'gallery-modal'
-  this.modal.innerHTML =
-    '<div class="gallery-modal-spinner spinner">' +
-      '<div class="double-bounce1"></div>' +
-      '<div class="double-bounce2"></div>' +
-    '</div>' +
-    '<div class="gallery-modal-image-wrapper"><img class="gallery-modal-image"></div>'
-  document.body.appendChild(this.modal)
-
-  this.modal.addEventListener('click', (function (event) {
-    if (event.target == this.modal) {
-      this.modal.classList.remove('active')
-    }
-  }).bind(this))
-
-  this.imageWrapper = document.querySelector('.gallery-modal-image-wrapper')
-  this.spinner = document.querySelector('.gallery-modal-spinner')
-
-  this.image = document.querySelector('.gallery-modal-image')
-  this.image.addEventListener('load', (function () {
-    this.spinner.style.zIndex = -1
-  }).bind(this))
-}
-
-GalleryColumns.prototype.showImage = function (photo) {
-  this.modal.classList.add('active')
-
-  if (this.image.src != photo.image.large) {
-    var windowAspectRatio = innerWidth / innerHeight
-    
-    if (windowAspectRatio > photo.aspect_ratio) {
-      this.imageWrapper.style.width = (innerHeight - 100) * photo.aspect_ratio + 'px'
-    } else {
-      this.imageWrapper.style.width = (innerWidth - 100) + 'px'
-      this.imageWrapper.style.marginTop = (innerHeight - (innerWidth - 100) / photo.aspect_ratio) / 2 + 'px'
-    }
-
-    this.image.src = photo.image.large
-    this.spinner.style.zIndex = 1
-  }
-}
-
 GalleryColumns.prototype.append = function (photos) {
   photos.forEach((function (photo) {
     var item = document.createElement('div')
     item.className = 'gallery-item'
     item.innerHTML =
       '<div class="gallery-photo">' +
-        '<img class="gallery-image" src="' + photo.image.small + '">' +
+        '<img class="gallery-image" data-large="' + photo.image.large + '" src="' + photo.image.small + '">' +
       '</div>' +
       '<div class="gallery-photo-info">' +
         '<div class="gallery-photo-name">' + photo.name + '</div>' +
@@ -80,7 +35,6 @@ GalleryColumns.prototype.append = function (photos) {
       '</div>'
     this.getMinColumn().appendChild(item)
     item.querySelector('.gallery-photo').style.height = parseInt(item.clientWidth / photo.aspect_ratio) + 'px'
-    item.querySelector('.gallery-image').addEventListener('click', this.showImage.bind(this, photo))
   }).bind(this))
 }
 
